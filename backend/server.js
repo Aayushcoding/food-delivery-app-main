2afe75217d959e78d3ec421918b4335e447cb963
Your Name <you@example.com>
2026-04-10 07:47:58 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorHandler');
 
 // Load environment variables
 dotenv.config();
-
-// Connect to database
-connectDB();
 
 const app = express();
 
